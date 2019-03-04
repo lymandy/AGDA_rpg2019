8492cc8854d3e42ec0d7d7b1b7f52aaa49844bfb
lymandy <lymandy97@gmail.com>
2019-03-04 03:04:10 -0700

    i tried

---
+++ RPG_AGDA_2019/Assets/Script/script.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class script : MonoBehaviour
+{
+    public void Options()
+    {
+        SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
