58a1da5075eddbf157b12425b21be531c597e9d4
lymandy <lymandy97@gmail.com>
2019-03-05 17:00:58 -0700

    improved level select ,add more scripts

---
+++ RPG_AGDA_2019/Assets/Script/LevelSelect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour
+{
+    public void BackToMainMenu()
+    {
+
+        SceneManager.LoadScene(2);
+    }
+
+    public void FirstLevel()
+    {
+        SceneManager.LoadScene(3);
+    }
+}
