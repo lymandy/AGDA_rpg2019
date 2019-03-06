5408948fa0fb627906e05a7383ba43ab860531c9
lymandy <lymandy97@gmail.com>
2019-03-05 17:34:49 -0700

    improved battle system

---
+++ RPG_AGDA_2019/Assets/Script/GameStateMachine.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStateMachine : MonoBehaviour
+{
+
+    public void BackToMainMenu()
+    {
+
+        SceneManager.LoadScene(2);
+    }
+
+    public void FirstLevel()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void BackToLevelSelect()
+    {
+
+        SceneManager.LoadScene(5);
+    }
+
+}
